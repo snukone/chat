79677513ee3a2281d9c7a320d1bf691f4b78fd01
snukone <simon.ernst@redwire.consulting>
2022-04-01 20:25:55 +0200

    Implement loginHandler for login process

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # chat
+
+option pattern: makes parameters in structs optional; implemented in trace-module
+decorator pattern: add additional functionality to handlers
+wrapper handler: also the same usage as decorator pattern
+chaining pattern: making handlers dependend of each other
--- main.go
+++ main.go
@@ -34,7 +34,8 @@ func main() {
 	flag.Parse() // parse the flags
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
