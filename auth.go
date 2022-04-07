31e6dc9ccafc0643d32d748f2415251cd1e48050
snukone <simon.ernst@redwire.consulting>
2022-04-07 19:55:03 +0200

    Adding unit test for ServeHTTP of auth type

--- auth.go
+++ auth.go
@@ -18,7 +18,7 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
 		// not authenticated
 		w.Header().Set("Location", "/login")
-		w.WriteHeader(http.StatusTemporaryRedirect)
+		w.WriteHeader(http.StatusPermanentRedirect)
 	} else if err != nil {
 		// some other error
 		panic(err.Error())
--- auth_test.go
+++ auth_test.go
@@ -9,6 +9,31 @@ import (
 	"github.com/stretchr/gomniauth/providers/github"
 )
 
+func TestServeHTTP(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	ah := &authHandler{
+		next: http.HandlerFunc(nil),
+	}
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	ah.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusPermanentRedirect {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusTemporaryRedirect)
+	}
+}
+
 func TestLoginHandler(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
 	// pass 'nil' as the third parameter.
