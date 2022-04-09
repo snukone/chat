9d120a86b51e5fe2e8291391f9d461570340b0d9
snukone <simon.ernst@redwire.consulting>
2022-04-09 09:01:28 +0200

    Activate ServeHTTP testing

--- auth_test.go
+++ auth_test.go
@@ -10,30 +10,30 @@ import (
 )
 
 // Already tested via TestMustAuth
-// func TestServeHTTP(t *testing.T) {
-// 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-// 	// pass 'nil' as the third parameter.
-// 	req, err := http.NewRequest("GET", "/", nil)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+func TestServeHTTP(t *testing.T) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-// 	rr := httptest.NewRecorder()
-// 	ah := &authHandler{
-// 		next: http.HandlerFunc(nil),
-// 	}
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	ah := &authHandler{
+		next: http.HandlerFunc(nil),
+	}
 
-// 	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-// 	// directly and pass in our Request and ResponseRecorder.
-// 	ah.ServeHTTP(rr, req)
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	ah.ServeHTTP(rr, req)
 
-// 	// Check the status code is what we expect.
-// 	if status := rr.Code; status != http.StatusTemporaryRedirect {
-// 		t.Errorf("handler returned wrong status code: got %v want %v",
-// 			status, http.StatusTemporaryRedirect)
-// 	}
-// }
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusTemporaryRedirect {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusTemporaryRedirect)
+	}
+}
 
 func TestMustAuth(t *testing.T) {
 	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
